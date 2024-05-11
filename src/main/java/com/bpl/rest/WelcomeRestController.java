9dbbc30f9536dc51fcf4e57cae11c4082188db65
Ranjit3168 <ranjit.sahoo@bluepal.com>
2024-05-11 15:31:04 +0530

    second commite

--- src/main/java/com/bpl/rest/WelcomeRestController.java
+++ src/main/java/com/bpl/rest/WelcomeRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeRestController {
 
-	@GetMapping("/")
+	@GetMapping("/welcome")
 	public String welcomeMsg() {
 		return "Welcome To Hyderabad";
 	}
