eb4bfe947adb26cadde6303b45a4d2e7f79dc3b0
Ranjit3168 <ranjit.sahoo@bluepal.com>
2024-05-11 12:34:27 +0530

    initial commit

---
+++ src/test/java/com/bpl/BootProj01DockerizingApp011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj01DockerizingApp011ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
